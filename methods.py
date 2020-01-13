d62ef828db7c8bcd7e39df628b7b9999a8115bcb
Emilio Araos <ea3dev@gmail.com>
2020-01-12 19:35:51 -0700

    args and kwargs

--- methods.py
+++ methods.py
@@ -59,11 +59,40 @@ def pig_latin(word):
 
 print(pig_latin("apple"))
 
+def myfunc(a,b):
+	#Returns 5% of the sum of a and b
+	return sum((a,b)) * 0.05
+print(myfunc(40,60))
 
 
+def myfunction(*args):
+	print(args)
+	for item in args:
+		print (item)
+
+myfunction(40,60,100,1,34)
 
 
+def myfunc(**kwargs):
+	print(kwargs)
+	if 'fruit' in kwargs:
+		print('My fruit of choice is {}'.format(kwargs['fruit']))
+	else:
+		print('I did not find any fruit here')
 
+
+myfunc(fruit='apple', veggie= 'lettuce')
+
+
+def myfunc(*args, **kwargs):
+	print(args)
+	print(kwargs)
+	print('I would like {} {}'.format(args[0], kwargs['food']))
+
+myfunc(10,20,30, fruit='orange', food='eggs', animal='dogs')
+
+
+ 
 
 
 
