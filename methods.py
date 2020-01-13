2195c4c305dd69a38517921bba5192582048df08
Emilio Araos <ea3dev@gmail.com>
2020-01-12 18:11:50 -0700

    default argument

---
+++ methods.py
@@ -0,0 +1,48 @@
+mylist = [1,2,3]
+mylist.append(4)
+print(mylist)
+mylist.pop()
+print(mylist)
+mylist.reverse()
+print(mylist)
+#help(mylist.insert)
+mylist.sort()
+print(mylist)
+
+##############################     FUNCTIONS!   ################################
+def name_function():
+	"""
+	DOCSTRING: Information about the function. 
+	Input: no input. 
+	OUTPUT: Hello
+	"""
+	print("Hello")
+
+name_function()
+
+def say_hello(name="NAME"):
+	print("HELLO " + name)
+
+say_hello("Emilio")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
